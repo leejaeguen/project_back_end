e700ab8b266f3d279f87cd80c749d1e1e0f2e04e
leejaeguen <129708727+leejaeguen@users.noreply.github.com>
2024-06-06 03:21:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import uvicorn
 
 app = FastAPI()
 
-origins=["http://127.0.0.1:5500", "http://54.147.247.233"]
+origins=["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,4 +25,4 @@ async def welcome() -> dict:
 app.include_router(todo_router)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=80, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
